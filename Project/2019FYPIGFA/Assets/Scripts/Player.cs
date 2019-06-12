650d4f4f0e3b3d072bc5902d10c9e082f0da4b24
Mohammad Rafiq <googaabumtum@gmail.com>
2019-06-12 17:11:48 +0800

    Renamed PlayerMoveController to Player. Mouse is now locked to screen and can be used to look around in game. Sprint (Left Ship/Input.Sprint in Editor) was added and health and stamina variables were added to player.

--- Project/2019FYPIGFA/Assets/Scripts/Player.cs
+++ Project/2019FYPIGFA/Assets/Scripts/Player.cs
@@ -2,21 +2,26 @@
 using System.Collections;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerMovementController : MonoBehaviour
+public class Player : MonoBehaviour
 {
     CharacterController characterController;
 
+    public float health = 10f, maxHealth = 10f, stamina = 10f, maxStamina = 10f;
     public float walkSpeed = 6.0f;
     public float sprintSpeedModifier = 1.25f;
     private float sprintSpeed = 7.5f;
     public float jumpSpeed = 6.0f;
     public float gravity = 20.0f;
     private bool doubleJump = false;
+    public Camera mainCamera;
+    public float mouseXSpeed = 2f, mouseYSpeed = 2f;
 
     private Vector3 moveDirection = Vector3.zero;
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         sprintSpeed = walkSpeed * sprintSpeedModifier;
         characterController = GetComponent<CharacterController>();
     }
@@ -27,10 +32,19 @@ public class PlayerMovementController : MonoBehaviour
         if (characterController.isGrounded)
         {
             doubleJump = false;
-            Vector3 localDir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            localDir *= walkSpeed;
-            moveDirection = transform.TransformDirection(localDir);
-
+            moveDirection = transform.TransformDirection(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            // Deplete stamina as you sprint/only activates if player is moving and holding sprint button
+            if(Input.GetButton("Sprint") && stamina > 0f && moveDirection.magnitude > 0f)
+            {
+                stamina = Mathf.Max(stamina - Time.deltaTime, 0f);
+                moveDirection *= sprintSpeed;
+            }
+            else
+            {
+                stamina = Mathf.Min(stamina + Time.deltaTime, maxStamina);
+                moveDirection *= walkSpeed;
+            }
+    
             if (Input.GetButtonDown("Jump"))
             {
                 doubleJump = true;
@@ -46,8 +60,10 @@ public class PlayerMovementController : MonoBehaviour
             }
         }
 
-        // Movement Update
+        // Movement/Look Update
         moveDirection.y -= gravity * Time.deltaTime;
         characterController.Move(moveDirection * Time.deltaTime);
+        transform.Rotate(0, mouseXSpeed * Input.GetAxis("Mouse X"), 0);
+        mainCamera.transform.Rotate(mouseYSpeed * Input.GetAxis("Mouse Y"), 0, 0);
     }
 }
