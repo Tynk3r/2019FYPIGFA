27b257b03245c99120d4dad967c7ca64b1b92d91
Alonzo Nalpon <judgementbearmaurice@gmail.com>
2019-07-16 09:51:14 +0800

    my tings

--- Project/2019FYPIGFA/Assets/Demo_files/Alonzo/Scripts/Generator/MapGeneratorEditor.cs
+++ Project/2019FYPIGFA/Assets/Demo_files/Alonzo/Scripts/Generator/MapGeneratorEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 
 [CustomEditor (typeof (MapGenerator))]
@@ -34,3 +35,4 @@ public class MapGeneratorEditor : Editor
         }
     }
 }
+#endif
