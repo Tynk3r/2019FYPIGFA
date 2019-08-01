2cc3d814bfcfb3de02986aaf672312dd0370eebe
Nalpon <judgementbearmaurice@gmail.com>
2019-08-01 17:07:39 +0800

    Fixed pizza slice projectile not hitting enemy (trigger was on) Enemies now play correct animations and damage comes when animation is complete Ragdolls now react and forces are applied when enemy dies to projectiles

--- Project/2019FYPIGFA/Assets/Scripts/P_PizzaSlice.cs
+++ Project/2019FYPIGFA/Assets/Scripts/P_PizzaSlice.cs
@@ -9,10 +9,12 @@ public class P_PizzaSlice : MonoBehaviour, I_Projectile
     public float EXPLOSIVE_RANGE = 5f;
     public float EXPLOSION_FORCE = 500f;
     public float rotationSpeed = 1f; // Only for aesthetics
-
+    public const float lingerTime = 3f;
     private bool m_playerOwned;
     private float m_lifeTime;
     private Rigidbody m_rb;
+
+    private bool m_live; // To make sure trail finishes before unrendering
 
     // Update is called once per frame
     public void Initialize(bool _playerOwned = false)
@@ -20,13 +22,24 @@ public class P_PizzaSlice : MonoBehaviour, I_Projectile
         m_lifeTime = MAX_LIFETIME;
         m_rb = GetComponent<Rigidbody>();
         m_playerOwned = _playerOwned;
+        m_live = true;
     }
     void Update()
     {
         transform.Rotate(0f, rotationSpeed, 0f, Space.Self);
         m_lifeTime -= Time.deltaTime;
-        if (m_lifeTime <= 0f)
-            Detonate();
+        if(m_live)
+        {
+            if (m_lifeTime <= 0f)
+                Detonate();
+        }
+        else
+        {
+            if (m_lifeTime <= 0f)
+            {
+                gameObject.SetActive(false);
+            }
+        }
     }
     public void Discharge(Vector3 _force, Vector3 _position)
     {
@@ -39,9 +52,10 @@ public class P_PizzaSlice : MonoBehaviour, I_Projectile
     public void Detonate(GameObject g)
     {
         // Check if it's an enemy. If it is, it takes damage
+        Debug.Log("Collided with an enemy");
         Enemy enemyHit = null;
         Player playerHit = null;
-        if (m_playerOwned)
+        if (!m_playerOwned)
             playerHit = g.GetComponent<Player>();
         else
             enemyHit = g.GetComponent<Enemy>();
@@ -49,25 +63,34 @@ public class P_PizzaSlice : MonoBehaviour, I_Projectile
         {
             // Apply force away from eggsplosion sorry i mean Pizzasplosio
             if (enemyHit.TakeDamage(damage))
-                enemyHit.GetComponent<Rigidbody>().AddExplosionForce(EXPLOSION_FORCE, transform.position, EXPLOSIVE_RANGE);
+            {
+                Rigidbody[] rigidbodies = enemyHit.GetComponentsInChildren<Rigidbody>();
+                foreach (Rigidbody rigidbody in rigidbodies)
+                {
+                    rigidbody.AddExplosionForce(EXPLOSION_FORCE, transform.position, EXPLOSIVE_RANGE);
+                }
+            }
         }
         else if (playerHit != null)
             playerHit.TakeDamage(damage);
-        gameObject.SetActive(false);
+        Detonate();
     }
+    
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag != "Player")
+        if (m_live)
             Detonate(collision.gameObject);
     }
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("trigger detected pizza");
         if (other.gameObject.tag != "Player")
             Detonate(other.gameObject);
     }
 
     public void Detonate()
     {
-        gameObject.SetActive(false);
+        m_live = false;
+        m_lifeTime = lingerTime;
     }
 }
